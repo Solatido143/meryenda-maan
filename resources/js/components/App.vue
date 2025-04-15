62bfde1f96a6b23a72ed05270e5fe22a4d147e98
Mark Anthony Sol Cruz <solcruzmarkanthony@gmail.com>
2025-04-15 09:57:56 +0800

    #

---
+++ resources/js/components/App.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="text-center mt-10 text-2xl text-blue-500">
+        Hello from Vue 3 inside Laravel 🎉
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped></style>
