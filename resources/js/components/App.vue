4dd181e51af1f90bbbdde988ac654f3708fbe9b4
Mark Anthony Sol Cruz <solcruzmarkanthony@gmail.com>
2025-04-27 21:18:31 +0800

    #

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,31 +1,4 @@
-<!-- <template>
-    <div>
-        <Navbar />
-
-        <main class="p-4">
-            <h2 class="text-xl font-semibold mb-2">Our Snacks</h2>
-            <ul>
-                <li v-for="snack in snacks" :key="snack.id" class="mb-1">
-                    🍡 {{ snack.name }}
-                </li>
-            </ul>
-        </main>
-
-        <Footer />
-    </div>
-</template>
-
-<script setup>
-import Navbar from "./layouts/Navbar.vue";
-import Footer from "./layouts/Footer.vue";
-
-const snacks = [
-    { id: 1, name: "Turon" },
-    { id: 2, name: "Banana Cue" },
-    { id: 3, name: "Kwek-kwek" },
-    { id: 4, name: "Fishball" },
-];
-</script> -->
+<style scoped></style>
 
 <template>
     <Main/>
