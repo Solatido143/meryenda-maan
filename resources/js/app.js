329978c1f3d00c761c876bd6266894ee091e9522
Mark Anthony Sol Cruz <solcruzmarkanthony@gmail.com>
2025-06-01 12:58:46 +0800

    clean router

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,18 +2,7 @@ import "./bootstrap";
 import "flowbite";
 
 import { createApp } from "vue";
-import { createRouter, createWebHistory } from "vue-router";
-
 import App from "./components/App.vue";
-import Dashboard from "./components/Dashboard.vue";
-
-// Define routes
-const routes = [{ path: "/dashboard", component: Dashboard }];
-
-// Create router instance
-const router = createRouter({
-    history: createWebHistory(),
-    routes,
-});
+import router from "./router/index.js";
 
 createApp(App).use(router).mount("#app");
---
+++ resources/js/router/index.js
@@ -0,0 +1,14 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+import Dashboard from "../components/Dashboard.vue";
+
+const routes = [
+    { path: "/dashboard", component: Dashboard}
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+});
+
+export default router;
